abbce19d6972e9ed105710ee1f6a8387e1af80ff
Teenapat <teenapat.wu@gmail.com>
2021-07-03 15:39:02 +0700

    ✨ service get put delete

--- src/app/shared/payment-detail.service.ts
+++ src/app/shared/payment-detail.service.ts
@@ -13,8 +13,25 @@ export class PaymentDetailService {
 
   formData:PaymentDetail = new PaymentDetail();
   readonly baseURL = 'http://localhost:5000/api/PaymentDetail'
+  list: PaymentDetail[]
 
   postPaymentDetail(){
     return this.http.post(this.baseURL, this.formData);
   }
+
+  putPaymentDetail(){
+    return this.http.put(`${this.baseURL}/${this.formData.paymentDetailId}`, this.formData);
+  }
+
+  deletePaymentDetail(id: number){
+    return this.http.delete(`${this.baseURL}/${id}`);
+  }
+
+  refreshList(){
+    this.http.get(this.baseURL)
+      .toPromise()
+      .then(res => this.list = res as PaymentDetail[])
+  }
+
+
 }
