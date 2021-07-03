e060b880bc6be7b56aeb5d93090aa78be48a8fc3
Teenapat <teenapat.wu@gmail.com>
2021-07-03 15:40:30 +0700

    ✨ populate form/delete by id

--- src/app/payment-details/payment-details.component.ts
+++ src/app/payment-details/payment-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {PaymentDetailService} from "../shared/payment-detail.service";
+import {PaymentDetail} from "../shared/payment-detail.model";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-payment-details',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public service: PaymentDetailService,
+    private toastrService: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.service.refreshList()
   }
 
+  populateForm(selectedRecord: PaymentDetail){
+    this.service.formData = Object.assign({}, selectedRecord)
+  }
+
+  onDelete(id: number) {
+    if ( confirm('Are you sure to delete this record?')){
+      this.service.deletePaymentDetail(id)
+        .subscribe(
+          res =>{
+            this.service.refreshList()
+            this.toastrService.error('Deleted successfully','Payment Detail Register')
+          },
+          error => {
+            console.log(error)
+          }
+        )
+    }
+  }
 }
